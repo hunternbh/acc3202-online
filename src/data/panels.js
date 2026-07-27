778beeaf35cdcf1bfa0c4d90b31db217f97b28bd
unknown <boonhianng@gmail.com>
2026-07-26 21:00:15 -0400

    Reduce course to five panels

--- src/data/panels.js
+++ src/data/panels.js
@@ -1,4 +1,4 @@
-// Edit these four objects to change the carousel cards and top panel titles.
+// Edit these five objects to change the carousel cards and top panel titles.
 export const heroImage = "./assets/background.png";
 
 export const panels = [
@@ -36,82 +36,10 @@ export const panels = [
   },
   {
     id: "lesson-5",
-    title: "Lesson 5: Under construction",
+    title: "Lesson 5",
     category: "Lesson 5 June 22nd 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-00-learning-joyfully.png",
-    imageAlt: "Under construction"
-  },
-  {
-    id: "lesson-6",
-    title: "Lesson 6: Under construction",
-    category: "Lesson 6 June 24th 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-01-what-is-ais.png",
-    imageAlt: "Under construction"
-  },
-  {
-    id: "lesson-7",
-    title: "Lesson 7: Under construction",
-    category: "Lesson 7 June 29th 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-02-business-analysts.png",
-    imageAlt: "Under construction"
-  },
-  {
-    id: "lesson-8",
-    title: "Lesson 8: Under construction",
-    category: "Lesson 8 July 1st 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-03-data-modelling.png",
-    imageAlt: "Under construction"
-  },
-  {
-    id: "lesson-9",
-    title: "Lesson 9: Under construction",
-    category: "Lesson 9 July 6th 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-00-learning-joyfully.png",
-    imageAlt: "Under construction"
-  },
-  {
-    id: "lesson-10",
-    title: "Lesson 10: Under construction",
-    category: "Lesson 10 July 8th 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-01-what-is-ais.png",
-    imageAlt: "Under construction"
-  },
-  {
-    id: "lesson-11",
-    title: "Lesson 11: Under construction",
-    category: "Lesson 11 July 13th 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-02-business-analysts.png",
-    imageAlt: "Under construction"
-  },
-  {
-    id: "lesson-12",
-    title: "Lesson 12: Under construction",
-    category: "Lesson 12 July 15th 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-03-data-modelling.png",
-    imageAlt: "Under construction"
-  },
-  {
-    id: "lesson-13",
-    title: "Lesson 13: Under construction",
-    category: "Lesson 13 July 20th 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-00-learning-joyfully.png",
-    imageAlt: "Under construction"
-  },
-  {
-    id: "lesson-14",
-    title: "Lesson 14: Under construction",
-    category: "Lesson 14 July 22nd 2026",
-    summary: "Under construction",
-    image: "./assets/carousel/panel-01-what-is-ais.png",
-    imageAlt: "Under construction"
+    summary: "Projects Review, Grading, and Course Progress",
+    image: "./assets/carousel/panel-04-relational-dbs-sql.png",
+    imageAlt: "Relational database and SQL concepts"
   }
 ];
